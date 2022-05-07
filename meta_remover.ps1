b29a6ab82885b95e1021c80643223310ed683d68
AlexJW <justWalsdi@protonmail.com>
2022-05-07 21:28:39 +0300

    gitignore comments added, meta_remover changed language

--- meta_remover.ps1
+++ meta_remover.ps1
@@ -2,15 +2,15 @@ Write-Output "-------"
 for ($i = 1; $i -le 12; $i++) {
   if ($i -lt 10) {
     $str_bin, $str_obj, $str_readme = ".\Lab_0$i\bin\", ".\Lab_0$i\obj\", ".\Lab_0$i\README.md"
-    try { Remove-Item -Path $str_bin -Recurse -Force -ErrorAction Stop; Write-Output "Bin 0$i удалён" } catch { Write-Output "Папка bin отсутствует."}
-    try { Remove-Item -Path $str_obj -Recurse -Force -ErrorAction Stop; Write-Output "Obj 0$i удалён" } catch { Write-Output "Папка obj отсутствует."}
-    try { Remove-Item -Path $str_readme -Force -ErrorAction Stop; Write-Output "README в папке Lab_0$i удалён" } catch { Write-Output "README в папке Lab_0$i отсутствует" }
+    try { Remove-Item -Path $str_bin -Recurse -Force -ErrorAction Stop; Write-Output "Bin 0$i removed" } catch { Write-Output "Folder bin is not present."}
+    try { Remove-Item -Path $str_obj -Recurse -Force -ErrorAction Stop; Write-Output "Obj 0$i removed" } catch { Write-Output "Folder obj is not present."}
+    try { Remove-Item -Path $str_readme -Force -ErrorAction Stop; Write-Output "README Lab_0$i removed" } catch { Write-Output "README Lab_0$i is not present" }
     Write-Output "-------"
   } else {
     $str_bin, $str_obj, $str_readme = ".\Lab_$i\bin\", ".\Lab_$i\obj\", ".\Lab_$i\README.md"
-    try { Remove-Item -Path $str_bin -Recurse -Force -ErrorAction Stop; Write-Output "Bin $i удалён" } catch { Write-Output "Папка bin отсутствует."}
-    try { Remove-Item -Path $str_obj -Recurse -Force -ErrorAction Stop; Write-Output "Obj $i удалён" } catch { Write-Output "Папка obj отсутствует."}
-    try { Remove-Item -Path $str_readme -Force -ErrorAction Stop; Write-Output "README в папке Lab_$i удалён" } catch { Write-Output "README в папке Lab_$i отсутствует" }
+    try { Remove-Item -Path $str_bin -Recurse -Force -ErrorAction Stop; Write-Output "Bin $i removed" } catch { Write-Output "Folder bin is not present."}
+    try { Remove-Item -Path $str_obj -Recurse -Force -ErrorAction Stop; Write-Output "Obj $i removed" } catch { Write-Output "Folder obj is not present."}
+    try { Remove-Item -Path $str_readme -Force -ErrorAction Stop; Write-Output "README Lab_$i removed" } catch { Write-Output "README Lab_$i is not present" }
     Write-Output "-------"
   }
 }
