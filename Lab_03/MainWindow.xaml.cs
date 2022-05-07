e197aed559a9a5d21e5eade2eb08927917e613da
AlexJW <justWalsdi@protonmail.com>
2022-05-07 20:01:41 +0300

    small optimizations

--- Lab_03/MainWindow.xaml.cs
+++ Lab_03/MainWindow.xaml.cs
@@ -12,23 +12,16 @@ namespace Lab_03
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        public MainWindow() => InitializeComponent();
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             //чтение матрицы
             List<Matrix> matr = new List<Matrix>();
-            int val = matr.Count;
-            string[] RecArray;
-
             using (StreamReader sr = new StreamReader(@"D:\Projects\visualstudio_source\Resources\ProdRecept.txt"))
             {
-
                 while (!sr.EndOfStream)
                 {
-                    RecArray = sr.ReadLine().Split(',');
+                    string[] RecArray = sr.ReadLine().Split(',');
                     matr.Add(new Matrix() { NameSyrie = RecArray[0], Rashod1 = RecArray[1], Rashod2 = RecArray[2], Rashod3 = RecArray[3], Rashod4 = RecArray[4], Rashod5 = RecArray[5] });
                 }
 
@@ -39,10 +32,8 @@ namespace Lab_03
             }
 
             using (StreamWriter writer = new StreamWriter(@"D:\Projects\visualstudio_source\Resources\ProdReceptNew.txt"))
-            {
                 for (int i = 0; i < matr.Count; i++)
                     writer.WriteLine(matr[i].NameSyrie + "," + matr[i].Rashod1 + "," + matr[i].Rashod2 + "," + matr[i].Rashod3 + "," + matr[i].Rashod4 + "," + matr[i].Rashod5);
-            }
             MessageBox.Show("Матрица выведена в файл");
         }
 
